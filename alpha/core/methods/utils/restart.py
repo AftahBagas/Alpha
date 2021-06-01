d90bf6c06e8976ebcabdc6aa01dafac7d1b97e49
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 06:39:54 +0700

    Create restart.py

---
+++ alpha/core/methods/utils/restart.py
@@ -0,0 +1,33 @@
+# alfareza
+
+__all__ = ['Restart']
+
+import os
+import sys
+
+import psutil
+
+from alpha import logging
+from ...ext import RawClient
+
+_LOG = logging.getLogger(__name__)
+_LOG_STR = "<<<!  #####  %s  #####  !>>>"
+
+
+class Restart(RawClient):  # pylint: disable=missing-class-docstring
+    async def restart(self, update_req: bool = False) -> None:  # pylint: disable=arguments-differ
+        """ Restart the Abstract Alpha """
+        _LOG.info(_LOG_STR, "Restarting Alpha")
+        await self.stop()
+        try:
+            c_p = psutil.Process(os.getpid())
+            for handler in c_p.open_files() + c_p.connections():
+                os.close(handler.fd)
+        except Exception as c_e:  # pylint: disable=broad-except
+            print(_LOG_STR % c_e)
+        if update_req:
+            print(_LOG_STR % "Installing Requirements...")
+            os.system(  # nosec
+                "pip3 install -U pip && pip3 install --no-cache-dir -r requirements.txt")
+        os.execl(sys.executable, sys.executable, '-m', 'userge')  # nosec
+        sys.exit()
