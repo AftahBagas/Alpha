82e74585e0fef86e9f29c1b54e01daaefab314c7
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 22:58:58 +0700

    Create __main__.py

---
+++ test/__main__.py
@@ -0,0 +1,15 @@
+# alfareza
+
+import os
+
+from alpha import alpha
+
+
+async def _worker() -> None:
+    chat_id = int(os.environ.get("CHAT_ID") or 0)
+    type_ = 'unofficial' if os.path.exists("../alpha/plugins/unofficial") else 'main'
+    await alpha.send_message(chat_id, f'`{type_} build completed !`')
+
+if __name__ == "__main__":
+    alphaz.begin(_worker())
+    print('Alpha test has been finished!')
