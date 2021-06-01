305e17a85a1608ea643644ab045215d7c427cc00
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 06:10:44 +0700

    Create __init__.py

---
+++ alpha/core/__init__.py
@@ -0,0 +1,8 @@
+# Alfareza
+
+from pyrogram import filters  # noqa
+
+from .database import get_collection  # noqa
+from .ext import pool  # noqa
+from .types.bound import Message  # noqa
+from .client import Alpha  # noqa
