d70cc9b591ad4bd775f7bfee31e34d3e4c35b392
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 13:43:55 +0700

    Update repo.py

--- alpha/plugins/tools/repo.py
+++ alpha/plugins/tools/repo.py
@@ -10,7 +10,7 @@ async def see_repo(message: Message):
 __Repo Userbot__ 😈 **Alpha** 😈
     __Tahan lama sebagai seorang Serge__
     __The Userbot Plugins__
-• **Version** : `{get_version()}`
+• **Version** : `{get_versions()}`
 • **License** : {versions.__license__}
 • **Copyright** : {versions.__copyright__}
 • **Repo** : [Alpha]({Config.UPSTREAM_REPO})
