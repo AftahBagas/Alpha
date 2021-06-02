b80734274961ebb32f60ec54f3e2e946af6ad110
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 20:34:22 +0700

    Create type.py

---
+++ alpha/plugins/fun/type.py
@@ -0,0 +1,34 @@
+# alfareza
+
+import time
+import random
+
+from pyrogram.errors.exceptions import FloodWait
+
+from alpha import alpha, Message
+
+
+@alpha.on_cmd("type", about={
+    'header': "Simulate a typewriter",
+    'usage': "{tr}type [text]"})
+async def type_(message: Message):
+    text = message.input_str
+    if not text:
+        await message.err("input not found")
+        return
+    s_time = 0.1
+    typing_symbol = '|'
+    old_text = ''
+    await message.edit(typing_symbol)
+    time.sleep(s_time)
+    for character in text:
+        s_t = s_time / random.randint(1, 100)
+        old_text += character
+        typing_text = old_text + typing_symbol
+        try:
+            await message.try_to_edit(typing_text, sudo=False)
+            time.sleep(s_t)
+            await message.try_to_edit(old_text, sudo=False)
+            time.sleep(s_t)
+        except FloodWait as x_e:
+            time.sleep(x_e.x)
