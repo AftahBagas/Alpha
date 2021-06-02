2f4018dc0cc5a2f22d8a452aa7562fa95f5565d7
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 20:24:04 +0700

    Create convert.py

---
+++ alpha/plugins/fun/convert.py
@@ -0,0 +1,48 @@
+""" convert text """
+
+# alfareza
+
+from alpha import alpha, Message
+
+
+@alpha.on_cmd("small", about={
+    'header': "Make caps smaller",
+    'usage': "{tr}small [text | reply to msg]"})
+async def small_(message: Message):
+    """ text to small """
+    text = message.input_str
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    if not text:
+        await message.err("input not found")
+        return
+    await message.edit(text.translate(str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                                                    "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxʏᴢ")))
+
+
+@alpha.on_cmd("lower", about={
+    'header': "Convert text to lowwer",
+    'usage': "{tr}lower [text | reply to msg]"})
+async def lower_(message: Message):
+    """ text to lower """
+    text = message.input_str
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    if not text:
+        await message.err("input not found")
+        return
+    await message.edit(text.lower())
+
+
+@alpha.on_cmd("upper", about={
+    'header': "Convert text to upper",
+    'usage': "{tr}upper [text | reply to msg]"})
+async def upper_(message: Message):
+    """ text to upper """
+    text = message.input_str
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    if not text:
+        await message.err("input not found")
+        return
+    await message.edit(text.upper())
