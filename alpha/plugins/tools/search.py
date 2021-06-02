d34021cfcd4995e64db88a833848b69baa9e4a74
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 14:29:02 +0700

    Create search.py

---
+++ alpha/plugins/tools/search.py
@@ -0,0 +1,20 @@
+# alfareza
+
+from alpha import alpha, Message
+
+
+@alpha.on_cmd("s", about={
+    'header': "search commands in Alpha",
+    'examples': "{tr}s wel"}, allow_channels=False)
+async def search(message: Message):
+    cmd = message.input_str
+    if not cmd:
+        await message.err(text="Enter any keyword to search in commands")
+        return
+    found = [i for i in sorted(list(alpha.manager.enabled_commands)) if cmd in i]
+    out_str = '    '.join(found)
+    if found:
+        out = f"**--I found ({len(found)}) commands for-- : `{cmd}`**\n\n`{out_str}`"
+    else:
+        out = f"__command not found for__ : `{cmd}`"
+    await message.edit(text=out, del_in=0)
