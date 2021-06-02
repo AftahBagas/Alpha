4b47bb4516fada073c982e0865696cb8deef2ce1
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 10:36:16 +0700

    Update __init__.py

--- alpha/core/__init__.py
+++ alpha/core/__init__.py
@@ -1,5 +1,6 @@
 from pyrogram import filters 
 
 from .database import get_collection  
-from .ext import pool  
+from .ext import pool 
+from .types.bound import Message 
 from .client import Alpha
