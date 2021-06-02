3a911267bdd984ae1c169abc8d3a29e0b3633fe7
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 10:03:56 +0700

    Delete __init__.py

--- alpha/__init__.py
+++
@@ -1,8 +0,0 @@
-# alfareza
-
-from alpha.logger import logging  # noqa
-from alpha.config import Config, get_version  # noqa
-from alpha.core import (  # noqa
-    Alpha, filters, Message, get_collection, pool)
-
-alpha = Alpha()
