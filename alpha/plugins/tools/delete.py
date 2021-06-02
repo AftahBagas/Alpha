8cc4ca478f1e5350715e985c5cfe68610d170b6b
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 14:20:03 +0700

    Create delete.py

---
+++ alpha/plugins/tools/delete.py
@@ -0,0 +1,11 @@
+# alfareza
+
+from alpha import alpha, Message
+
+
+@alpha.on_cmd("del", about={'header': "delete replied message"})
+async def del_msg(message: Message):
+    msg_ids = [message.message_id]
+    if message.reply_to_message:
+        msg_ids.append(message.reply_to_message.message_id)
+    await message.client.delete_messages(message.chat.id, msg_ids)
