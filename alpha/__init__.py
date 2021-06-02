10c046d81f0c5cc15ee59ce703e1c10ec830424f
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 10:07:26 +0700

    Create __init__.py

---
+++ alpha/__init__.py
@@ -0,0 +1,6 @@
+from alpha.logger import logging  # noqa
+from alpha.config import Config, get_version  # noqa
+from alpha.core import (  # noqa
+    Alpha, filters, Message, get_collection, pool)
+
+alpha = Alpha()
