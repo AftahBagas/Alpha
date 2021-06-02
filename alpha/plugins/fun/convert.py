984fdd8b646b9f6ff6668d524ff893c0be6b0a07
AftahBagas <aftahbagas21@gmail.com>
2021-06-02 13:24:46 +0000

    auto: black code format

--- alpha/plugins/fun/convert.py
+++ alpha/plugins/fun/convert.py
@@ -2,29 +2,37 @@
 
 # alfareza
 
-from alpha import alpha, Message
+from alpha import Message, alpha
 
 
-@alpha.on_cmd("small", about={
-    'header': "Make caps smaller",
-    'usage': "{tr}small [text | reply to msg]"})
+@alpha.on_cmd(
+    "small",
+    about={"header": "Make caps smaller", "usage": "{tr}small [text | reply to msg]"},
+)
 async def small_(message: Message):
-    """ text to small """
+    """text to small"""
     text = message.input_str
     if message.reply_to_message:
         text = message.reply_to_message.text
     if not text:
         await message.err("input not found")
         return
-    await message.edit(text.translate(str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-                                                    "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxʏᴢ")))
+    await message.edit(
+        text.translate(
+            str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxʏᴢ")
+        )
+    )
 
 
-@alpha.on_cmd("lower", about={
-    'header': "Convert text to lowwer",
-    'usage': "{tr}lower [text | reply to msg]"})
+@alpha.on_cmd(
+    "lower",
+    about={
+        "header": "Convert text to lowwer",
+        "usage": "{tr}lower [text | reply to msg]",
+    },
+)
 async def lower_(message: Message):
-    """ text to lower """
+    """text to lower"""
     text = message.input_str
     if message.reply_to_message:
         text = message.reply_to_message.text
@@ -34,11 +42,15 @@ async def lower_(message: Message):
     await message.edit(text.lower())
 
 
-@alpha.on_cmd("upper", about={
-    'header': "Convert text to upper",
-    'usage': "{tr}upper [text | reply to msg]"})
+@alpha.on_cmd(
+    "upper",
+    about={
+        "header": "Convert text to upper",
+        "usage": "{tr}upper [text | reply to msg]",
+    },
+)
 async def upper_(message: Message):
-    """ text to upper """
+    """text to upper"""
     text = message.input_str
     if message.reply_to_message:
         text = message.reply_to_message.text
