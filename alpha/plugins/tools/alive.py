24b59122b069a6b31154eecead4ef7fe78b25fec
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-03 09:05:52 +0700

    Create alive.py

---
+++ alpha/plugins/tools/alive.py
@@ -0,0 +1,16 @@
+from alpha import Config, Message, alpha, versions
+
+
+@alpha.on_cmd("alive", about={"header": "get repo link and details"})
+async def see_repo(message: Message):
+    """see repo"""
+    output = f"""
+__Repo Userbot__ 😈 **Alpha** 😈
+    __Tahan lama sebagai seorang Serge__
+    __The Userbot Plugins__
+• **Version** : `0.5.1`
+• **License** : {versions.__license__}
+• **Copyright** : {versions.__copyright__}
+• **Repo** : [Alpha]({Config.UPSTREAM_REPO})
+"""
+    await message.edit(output)
