874c4dacb4d898c4649aa72e22a831522ac15b17
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-04 09:16:19 +0700

    Update repo.py

--- alpha/plugins/tools/repo.py
+++ alpha/plugins/tools/repo.py
@@ -3,7 +3,10 @@
 from alpha import Config, Message, alpha, versions
 
 
-@alpha.on_cmd("repo", about={"header": "get repo link and details"})
+@alpha.on_cmd("repo",about={
+        "header": "Repo Alpha Userbot",
+        "description": "Kirim Link Repo Dengan Detail\n\n"
+        "© [TheAlphaSupport](https://t.me/thealphasupport)}
 async def see_repo(message: Message):
     """see repo"""
     output = f"""
