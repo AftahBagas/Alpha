02f2c0f850623f37b59b278809fe4280d6d5e332
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-04 11:15:10 +0700

    Update alive.py

--- alpha/plugins/tools/alive.py
+++ alpha/plugins/tools/alive.py
@@ -34,8 +34,7 @@ _LOGO_ID = None
 
 
 @alpha.on_cmd(
-    "alive", about={"header": "This command is just for fun/n/n"
-    "© [TheAlphaSupport](https://t.me/TheAlphaSupport)"}, allow_channels=False
+    "alive", about={"header": "This command is just for fun"}, allow_channels=False
 )
 async def alive(message: Message):
     if not (_CHAT and _MSG_ID):
