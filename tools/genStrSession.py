adfb53d1db88341e90202bf49b4c2a49cc5ee1bb
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-05 12:18:46 +0700

    Update genStrSession.py

--- tools/genStrSession.py
+++ tools/genStrSession.py
@@ -1,9 +1,10 @@
-# alfareza
+# Alfareza
 
 import os
 import asyncio
 
 from pyrogram import Client
+from pyrogram.errors import UserIsBot
 from dotenv import load_dotenv
 
 if os.path.isfile("config.env"):
@@ -12,14 +13,21 @@ if os.path.isfile("config.env"):
 
 async def genStrSession() -> None:  # pylint: disable=missing-function-docstring
     async with Client(
-            "Alpha",
-            api_id=int(os.environ.get("API_ID") or input("Enter Telegram APP ID: ")),
-            api_hash=os.environ.get("API_HASH") or input("Enter Telegram API HASH: ")
+        "Alpha",
+        api_id=int(os.environ.get("API_ID") or input("Enter Telegram APP ID: ")),
+        api_hash=os.environ.get("API_HASH") or input("Enter Telegram API HASH: "),
     ) as alpha:
         print("\nprocessing...")
-        await alpha.send_message(
-            "me", f"#ALPHA #HU_STRING_SESSION\n\n```{await alphaz.export_session_string()}```")
-        print("Done !, session string has been sent to saved messages!")
+        doneStr = "sent to saved messages!"
+        try:
+            await userge.send_message(
+                "me", f"#ALPHA #HU_STRING_SESSION\n\n```{await alpha.export_session_string()}```"
+            )
+        except UserIsBot:
+            doneStr = "successfully printed!"
+            print(await alpha.export_session_string())
+        print(f"Done !, session string has been {doneStr}")
+
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(genStrSession())
