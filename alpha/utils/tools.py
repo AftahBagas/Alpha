d5b86a100438c611af198c5cdb77cb05a434fd76
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-05 07:42:21 +0700

    Update tools.py

--- alpha/utils/tools.py
+++ alpha/utils/tools.py
@@ -4,7 +4,7 @@ import re
 import shlex
 import asyncio
 from os.path import basename, join, exists
-from emoji import get_emoji_regexp
+from emoji import get_regexp
 from typing import Tuple, List, Optional, Iterator, Union
 
 from html_telegraph_poster import TelegraphPoster
