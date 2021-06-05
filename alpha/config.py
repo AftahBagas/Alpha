254478425758b93c7c44a835aca646af4a5912db
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-05 12:46:58 +0700

    Update config.py

--- alpha/config.py
+++ alpha/config.py
@@ -61,6 +61,7 @@ class Config:
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     G_DRIVE_IS_TD = os.environ.get("G_DRIVE_IS_TD") == "true"
     LOAD_UNOFFICIAL_PLUGINS = os.environ.get("LOAD_UNOFFICIAL_PLUGINS") == "true"
+    ASSERT_SINGLE_INSTANCE = os.environ.get("ASSERT_SINGLE_INSTANCE", "false").lower() == "true"
     THUMB_PATH = DOWN_PATH + "thumb_image.jpg"
     TMP_PATH = "alpha/plugins/temp/"
     MAX_MESSAGE_LENGTH = 4096
