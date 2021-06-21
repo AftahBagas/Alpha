4774735d56c70d4a2cbb21177c155e274c326f9c
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-21 16:23:11 +0700

    Update repo.py

--- alpha/plugins/tools/repo.py
+++ alpha/plugins/tools/repo.py
@@ -7,12 +7,10 @@ from alpha import Config, Message, alpha, versions
 async def see_repo(message: Message):
     """see repo"""
     output = f"""
-__Repo Userbot__ 😈 **Alpha** 😈
-    __Tahan lama Sebagai Seorang Alpha__
-    __The Userbot Plugins__
-• **Version** : `0.4.0`
-• **License** : {versions.__license__}
-• **Copyright** : {versions.__copyright__}
-• **Repo** : [Alpha]({Config.UPSTREAM_REPO})
+[Alpha](https://t.me/TeamSquadUserbotSupport) repo :
+  • **Version** : `0.4.0`
+  • **License** : {versions.__license__}
+  • **Copyright** : {versions.__copyright__}
+  • **Repo** : [Alpha]({Config.UPSTREAM_REPO})
 """
     await message.edit(output)
