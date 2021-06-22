192ca4c44612b70b1e40e8e11611d24986a4396b
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-22 14:16:49 +0700

    Update __init__.py

--- alpha/utils/__init__.py
+++ alpha/utils/__init__.py
@@ -1,12 +1,23 @@
 # alfareza
 
-from .progress import progress  # noqa
-from .sys_tools import SafeDict, get_import_path, terminate, secure_text  # noqa
-from .tools import (sort_file_name_key, # noqa
+from .progress import progress  
+from .sys_tools import SafeDict, get_import_path, terminate, secure_text
+from .functions import (
+    AttributeDict,
+    check_owner,
+    cleanhtml,
+    deEmojify,
+    escape_markdown,
+    media_to_image,
+    mention_html,
+    mention_markdown,
+    rand_array,
+    rand_key,
+    thumb_from_audio,
+)  
+from .tools import (sort_file_name_key, 
                     get_file_id_of_media,
                     demojify,
-                    mention_html,
-                    rand_array,
                     humanbytes,
                     time_formatter,
                     post_to_telegraph,
