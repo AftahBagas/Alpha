2cc2a7fc182c7b3bf3f3afc53b709d9ee47a11ca
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-22 13:38:05 +0700

    Update __init__.py

--- alpha/utils/__init__.py
+++ alpha/utils/__init__.py
@@ -4,7 +4,8 @@ from .progress import progress  # noqa
 from .sys_tools import SafeDict, get_import_path, terminate, secure_text  # noqa
 from .tools import (sort_file_name_key, # noqa
                     get_file_id_of_media,
-                    demojify,
+                    deEmojify,
+                    mention_html,
                     rand_array,
                     humanbytes,
                     time_formatter,
