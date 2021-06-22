8a29b68fe95e7efacd91fc2a39d83761bfb915f5
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-22 12:45:01 +0700

    Update repo.py

--- alpha/plugins/tools/repo.py
+++ alpha/plugins/tools/repo.py
@@ -7,7 +7,8 @@ from alpha import Config, Message, alpha, versions, get_version
 async def see_repo(message: Message):
     """see repo"""
     output = f"""
-[Alpha](https://t.me/TeamSquadUserbotSupport) repo :
+Repository Alpha
+
   • **Version** : {get_version()}
   • **License** : {versions.__license__}
   • **Copyright** : {versions.__copyright__}
