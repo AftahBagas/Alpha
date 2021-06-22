867d7a6995d4b5dc944c790b63531c293e627610
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-22 14:42:41 +0700

    Update help.py

--- alpha/plugins/help.py
+++ alpha/plugins/help.py
@@ -52,7 +52,7 @@ async def helpme(
     plugins = alpha.manager.enabled_plugins
     if not message.input_str:
         out_str = (
-            f"""📚 <b><u>(<code>{len(plugins)}</code>) Plugin(s) Available</u></b>\n\n"""
+            f"""📚 <b><u>(<code>{len(plugins)}</code>) Total Plugin(s) Available</u></b>\n\n"""
         )
         cat_plugins = alpha.manager.get_plugins()
         for cat in sorted(cat_plugins):
