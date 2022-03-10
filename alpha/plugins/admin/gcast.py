c5893e0c4338e00e95242daa83c7dc20e6d65da8
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2022-03-10 12:27:43 +0700

    Update gcast.py

--- alpha/plugins/admin/gcast.py
+++ alpha/plugins/admin/gcast.py
@@ -1,3 +1,4 @@
+
 from pyrogram.errors import (
     FloodWait,
 )
@@ -13,22 +14,22 @@ Gblacklist = [-1001159103924, -1001718757023]
 
 @alpha.on_cmd("gcast", about={
     'header': "gcast chat",
-    'alpha': "{tr}gcast text",
+    'alpha': "{tr}gcast reply tomessage ",
     'examples': "{tr}gcast"})
 async def gcast(message: Message):
-    xx = message.text
+    xx = message.reply_to_message
     if xx:
         msg = xx
     elif message.reply_to_message:
         msg = await alpha.get_reply_message()
     else:
         return await message.edit("`Berikan Sebuah Pesan atau Reply`")
-    alfa = await message.edit("`Globally Broadcasting Msg...`")
+    kk = await message.edit("`Globally Broadcasting Msg...`")
     er = 0
     done = 0
     async for x in alpha.iter_dialogs():
                 try:
-                    await alpha.send_message(chat, msg)
+                    await alpha.send_message(x.chat.id, msg)
                     await asyncio.sleep(0.1)
                     done += 1
                 except FloodWait as anj:
@@ -37,6 +38,6 @@ async def gcast(message: Message):
                     done += 1
                 except BaseException:
                     er += 1
-    await alfa.edit(
-        f"_Sukses Mengirim Pesan Ke_ `{done}` _Grup, Gagal Mengirim Pesan Ke_ `{er}` _Grup_"
+    await kk.edit(
+        f"_Berhasil Mengirim Pesan Ke_ `{done}` _Grup, Gagal Mengirim Pesan Ke_ `{er}` _Grup_"
     )
